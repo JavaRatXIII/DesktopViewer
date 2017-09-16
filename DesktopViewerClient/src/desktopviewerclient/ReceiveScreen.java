101a09cf00a337a800c33690d8b8d03c08332da2
JavaRatXIII <tamannaqureshi9@gmail.com>
2017-09-16 18:32:17 +0100

    Ajusted ReciveScreen class

--- DesktopViewerClient/src/desktopviewerclient/ReceiveScreen.java
+++ DesktopViewerClient/src/desktopviewerclient/ReceiveScreen.java
@@ -5,44 +5,45 @@ import java.awt.Image;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import Console.*;
 
-class ReceiveScreen extends Thread{
-	private ObjectInputStream cObjectInputStream = null;
-	private JPanel cPanel = null;
-	private boolean continueLoop = true;
-	InputStream oin = null;
-	Image image1 = null;
+class ReceiveScreen extends Thread
+{
+    private JPanel _clientPanel = null;
+    private InputStream _clinetInput = null;
+    private Image _clientImage = null;
 
-	public ReceiveScreen(InputStream in,JPanel p){
-		oin = in;
-		cPanel = p;
-		start();
-	}
+    public ReceiveScreen(InputStream input,JPanel clientPanel){
+        _clinetInput = input;
+        _clientPanel = clientPanel;
+        start();
+    }
 
-	public void run(){
-		try{
-			//Read screenshots of the client and then draw them
-			while(continueLoop){
-				byte[] bytes = new byte[1024*1024];
-				int count = 0;
-				do{
-					count+=oin.read(bytes,count,bytes.length-count);
-				}while(!(count>4 && bytes[count-2]==(byte)-1 && bytes[count-1]==(byte)-39));
+    public void run()
+    {
+        try
+        {
+            while(true)
+            {
+                byte[] bytes = new byte[1024*1024];
+                int count = 0;
+                
+                while(!(count>4 && bytes[count-2]==(byte)-1 && bytes[count-1]==(byte)-39))
+                {
+                    count+=_clinetInput.read(bytes,count,bytes.length-count);
+                }
 
-				image1 = ImageIO.read(new ByteArrayInputStream(bytes));
-				image1 = image1.getScaledInstance(cPanel.getWidth(),cPanel.getHeight(),Image.SCALE_FAST);
-
-				//Draw the received screenshots
-
-				Graphics graphics = cPanel.getGraphics();
-				graphics.drawImage(image1, 0, 0, cPanel.getWidth(), cPanel.getHeight(), cPanel);
-			}
-
-		} catch(IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+                _clientImage = ImageIO.read(new ByteArrayInputStream(bytes));
+                _clientImage = _clientImage.getScaledInstance(_clientPanel.getWidth(),_clientPanel.getHeight(),Image.SCALE_FAST);
+                Graphics graphics = _clientPanel.getGraphics();
+                graphics.drawImage(_clientImage, 0, 0, _clientPanel.getWidth(), _clientPanel.getHeight(), _clientPanel);
+            }
+        } 
+        catch(IOException ex) 
+        {
+            new Console().WriteLine(ex.getMessage());
+        }
+    }
 }
