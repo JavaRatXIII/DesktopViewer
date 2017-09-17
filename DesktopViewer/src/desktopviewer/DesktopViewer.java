61478c596adba1255866e4ad447d2d581bf2f8ff
JavaRatXIII <tamannaqureshi9@gmail.com>
2017-09-17 22:43:41 +0100

    Improved server main class

--- DesktopViewer/src/desktopviewer/DesktopViewer.java
+++ DesktopViewer/src/desktopviewer/DesktopViewer.java
@@ -6,12 +6,11 @@ package desktopviewer;
  */
 public class DesktopViewer 
 {
-    public static String port="1204";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new StartServer(Integer.parseInt(port));
+        new StartServer(Integer.parseInt("1204"));
     }
     
 }
